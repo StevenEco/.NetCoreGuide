458dd777f9d4ebf87583365e5ef77f64f86ad28a
WarrenRyan <cxtionch@live.com>
2019-08-25 00:20:45 +0800

    0825update，更新不完全

--- Code/CoreStudy03面向对象/Program.cs
+++ Code/CoreStudy03面向对象/Program.cs
@@ -1,26 +1,14 @@
 ﻿using System;
-
+using System.Text;
 namespace CoreStudy03面向对象
 {
-    struct Test
-    {
-        public int x;
-        public void test(int x)
-        {
-            this.x = x;
-        }
-    }
 
     class Program
     {
         static void Main(string[] args)
         {
-
-            Test t = new Test();
-            t.x = 100;
-            object a = t;//装箱
-            ((Test)a).test(300);//x还是100不变，为什么
-            Console.WriteLine(((Test)a).x);
+            var temp = ('b'+'a'+ 'a' + 'a').ToString();
+            Console.WriteLine(temp);
         }
     }
 }
